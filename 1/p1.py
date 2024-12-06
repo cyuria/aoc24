ff375f1e2486e44ca90daf5320c231dc5df0ef20
Cyuria <cyuria.dev@gmail.com>
2024-12-06 16:54:22 +1100

    day 1

---
+++ 1/p1.py
@@ -0,0 +1,4 @@
+with open('input.txt', 'r') as f:
+    left, right = zip(*([int(n) for n in line.split(' ')] for line in f.read().split('\n') if line))
+
+print(sum(abs(a - b) for a, b in zip(sorted(left), sorted(right))))
